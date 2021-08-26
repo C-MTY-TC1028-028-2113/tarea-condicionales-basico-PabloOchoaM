d57fbb2ff48448063efcee60084d008198af9096
PabloOchoaM <a01067378@itesm.mx>
2021-08-26 15:03:53 -0500

    Trabajos terminados

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,21 @@
 
 def main():
     edad = int(input("Ingresa tu edad: "))
+    
     #Aquí empieza tu programa...
+    if edad >= 18:
+        ID= str(input("¿Tienes identificación oficial? (s/n): "))
+        if ID=="s" or ID=="S":
+            print("Trámite de licencia concedido")
+        elif ID=="n" or ID=="N":
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+    elif edad<18 and edad>0:
+        print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")
+
 
 if __name__ == '__main__':
     main()
